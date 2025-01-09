716014c74192acef54185335938f7d80f3e93829
Avy <gibraltar.av@gmail.com>
2025-01-09 15:11:36 +0800

    Add practice and event objects to calendar api
    home.views import Practice and Event from
    scheduler.models to supplement HomeView with
    Practice and Event objects to FullCalendar API

--- website/home/views.py
+++ website/home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
+from scheduler.models import Practice, Event
+
 class HomeView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Supplement additional data to frontend html for calendar
+        context["event_list"] = Event.objects.all()
+        context["practice_list"] = Practice.objects.all()
+        return context
+    
