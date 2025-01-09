d069546ed20faa599a1b3a9895fb4c8bf3d2991c
Avy <gibraltar.av@gmail.com>
2025-01-10 00:26:49 +0800

    Add base create form structure and path

--- website/scheduler/urls.py
+++ website/scheduler/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name = "scheduler"
 urlpatterns = [
     path('', PracticeListView.as_view(), name="practice-all"),
+    path('add', PracticeCreateView.as_view(), name="practice-create"),
 ]
--- website/scheduler/views.py
+++ website/scheduler/views.py
@@ -1,6 +1,20 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import Practice
 
 class PracticeListView(ListView):
     model = Practice
     template_name = "scheduler/list.html"
+
+class PracticeCreateView(CreateView):
+    model = Practice
+    fields = "__all__"
+    template_name = "scheduler/form.html"
+
+class PracticeUpdateView(UpdateView):
+    model = Practice
+    fields = "__all__"
+    template_name = "scheduler/form.html"
+
+class PracticeDeleteView(DeleteView):
+    model = Practice
+    template_name = "scheduler/delete_confirm.html"
