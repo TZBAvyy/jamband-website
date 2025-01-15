ac7040d9a548361fe1ed4604b9e57af0d3cb077e
Avy <gibraltar.av@gmail.com>
2025-01-15 23:18:05 +0800

    Add django-extentions and scripting capabilities
    Add script to convert csv table of members into
    Member model info

--- website/scripts/init_members_table.py
+++ website/scripts/init_members_table.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 from decouple import config
 
 def run():
-    with open("hidden/AY24_25 Members Details.csv", 'r') as file:
+    with open("hidden/AY24_25-Members-Details.csv", 'r') as file:
         reader = csv.DictReader(file)
         print("File Opened")
 
         #Reset Member and User Table
         Member.objects.all().delete()
         User.objects.all().delete()
+        print("Delete complete, starting user creation")
 
         #Create default user and superuser (REMEMBER TO UPDATE .env FILE)
         user = User.objects.create_user(
@@ -27,3 +28,9 @@ def run():
             )
         superuser.first_name = "CoChair Account"
         superuser.save()
+        print("Create users completed, starting member insertion")
+
+        for row in reader:
+            member = Member.objects.get_or_create(name=row["Name  (Full as per Matric)"])[0]
+                
+    print("Script completed")
